9c33636c3616838b4ac5a642fe41d5e70be6b795
VytskaLT <VytskaLT@protonmail.com>
2022-03-16 17:19:12 +0200

    Change namespace

--- api/src/main/java/net/megavex/scoreboardlibrary/api/ScoreboardLibrary.java
+++ api/src/main/java/net/megavex/scoreboardlibrary/api/ScoreboardLibrary.java
@@ -3,7 +3,7 @@ package net.megavex.scoreboardlibrary.api;
 public final class ScoreboardLibrary {
 
   // Concat is used to trick package relocations
-  public static final String NAMESPACE = "net.vytska".concat("lt.scoreboardlibrary");
+  public static final String NAMESPACE = "net.mega".concat("vex.scoreboardlibrary");
 
   private ScoreboardLibrary() {
   }
