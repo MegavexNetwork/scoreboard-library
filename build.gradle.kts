f29ea8a077c556d42bbf985e0312798cf76bcd7f
VytskaLT <VytskaLT@protonmail.com>
2023-01-08 19:19:16 +0200

    Change version to 2.0.0-RC3

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
 }
 
 allprojects {
-  version = "2.0.0-RC2"
+  version = "2.0.0-RC3"
   group = "net.megavex.scoreboardlibrary"
   description = "Powerful packet-level Scoreboard library for Paper/Spigot servers"
 
