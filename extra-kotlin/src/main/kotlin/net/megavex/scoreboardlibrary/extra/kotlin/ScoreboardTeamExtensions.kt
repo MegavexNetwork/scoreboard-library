98b5cd9a3bbfaaf9402701a3f5a5812b0af3dd8e
VytskaLT <VytskaLT@protonmail.com>
2023-01-22 17:06:10 +0200

    Make some Kotlin extensions `inline`

--- extra-kotlin/src/main/kotlin/net/megavex/scoreboardlibrary/extra/kotlin/ScoreboardTeamExtensions.kt
+++ extra-kotlin/src/main/kotlin/net/megavex/scoreboardlibrary/extra/kotlin/ScoreboardTeamExtensions.kt
@@ -5,8 +5,8 @@ import net.megavex.scoreboardlibrary.api.team.TeamInfo
 import net.megavex.scoreboardlibrary.api.team.TeamManager
 import org.bukkit.entity.Player
 
-public val ScoreboardTeam.teamManager: TeamManager get() = teamManager()
-public val ScoreboardTeam.name: String get() = name()
-public val ScoreboardTeam.globalInfo: TeamInfo get() = globalInfo()
+public inline val ScoreboardTeam.teamManager: TeamManager get() = teamManager()
+public inline val ScoreboardTeam.name: String get() = name()
+public inline val ScoreboardTeam.globalInfo: TeamInfo get() = globalInfo()
 public operator fun ScoreboardTeam.get(player: Player): TeamInfo = teamInfo(player)
 public operator fun ScoreboardTeam.set(player: Player, teamInfo: TeamInfo): Unit = teamInfo(player, teamInfo)
--- extra-kotlin/src/main/kotlin/net/megavex/scoreboardlibrary/extra/kotlin/TeamManagerExtensions.kt
+++ extra-kotlin/src/main/kotlin/net/megavex/scoreboardlibrary/extra/kotlin/TeamManagerExtensions.kt
@@ -3,5 +3,5 @@ package net.megavex.scoreboardlibrary.extra.kotlin
 import net.megavex.scoreboardlibrary.api.team.ScoreboardTeam
 import net.megavex.scoreboardlibrary.api.team.TeamManager
 
-public val TeamManager.teams: Collection<ScoreboardTeam> get() = teams();
+public inline val TeamManager.teams: Collection<ScoreboardTeam> get() = teams()
 public operator fun TeamManager.get(name: String): ScoreboardTeam? = team(name)
