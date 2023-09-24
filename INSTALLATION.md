cc6eae07ee8824aac85385693323d4e368dad794
vytskalt <vytskalt@protonmail.com>
2023-09-24 17:11:21 +0300

    Release 2.0.0-RC13

--- INSTALLATION.md
+++ INSTALLATION.md
@@ -1,6 +1,6 @@
 # Installation
 
-Latest version: `2.0.0-RC12`
+Latest version: `2.0.0-RC13`
 
 ## Gradle
 
--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
 }
 
 allprojects {
-  version = "2.0.0-RC12"
+  version = "2.0.0-RC13"
   group = "net.megavex"
   description = "Powerful packet-level sidebar/teams library for Paper/Spigot servers"
 
