b98e1f007b44f562ac930cddbfdd9ed5d5b6fcdc
vytskalt <vytskalt@protonmail.com>
2024-01-23 17:54:00 +0200

    Release 2.0.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
 }
 
 allprojects {
-  version = "2.0.1"
+  version = "2.0.2"
   group = "net.megavex"
   description = "Powerful packet-level sidebar/teams library for Paper/Spigot servers"
 
