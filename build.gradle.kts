77dd1b8c654350cd3563c0e26df1a47bac5886f3
vytskalt <vytskalt@protonmail.com>
2024-03-07 16:47:13 +0200

    release 2.1.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
 }
 
 allprojects {
-  version = "2.1.1-SNAPSHOT"
+  version = "2.1.1"
   group = "net.megavex"
   description = "Powerful packet-level scoreboard library for Paper/Spigot servers"
 
